e177c017a19e5912a9dd2ce5b814d524d212d757
Jumper Luko <jumper.luko@gmail.com>
2019-09-18 13:09:20 -0300

    + footer

--- footer.php
+++ footer.php
@@ -1,3 +1,18 @@
     <?php wp_footer(); ?>
+    <footer class="garow-5 gcol--2">
+        <a class="link1" href="" >Página Inicial</a>
+        <a class="contact1" href="" >Redes Sociais</a>
+
+        <a class="link2" href="" >Como Escolher</a>
+        <a class="contact2" href="" >Whatsapp: +55 47 98479-0989</a>
+
+        <a class="link3" href="" >Certificações</a>
+        <a class="contact3" href="" >Email: comercial.marecia@gmail.com</a>
+
+        <a class="link4" href="" >Dúvidas</a>
+        <a class="contact4" href="" >Telefone: (47) 3026-5141</a>
+
+        <a class="andress" href="" >Rua Prefeito Helmuth Fallgatter, 1208  - 89206-100 – Boa Vista – Joinville/SC</a>
+    </footer>
     </body>
 </html>
