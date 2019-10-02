d1912693d146a84d8d158d3d1075569de66beb20
Jumper Luko <jumper.luko@gmail.com>
2019-10-02 11:59:18 -0300

    Amém está funcionando no sentido que queria

--- page-produtos.php
+++ page-produtos.php
@@ -1,9 +1,18 @@
 <?php get_header(); ?>
 <link rel="stylesheet" type="text/css" href="<?php echo get_parent_theme_file_uri( 'css/produtos.min.css' );?>">
 <main class="">
-    <?php the_title();
-    echo do_shortcode("[catlist name='Produtos']");
-    ?>
+
+<!-- https://www.wpbeginner.com/wp-tutorials/how-to-display-recent-posts-from-a-specific-category-in-wordpress/ -->
+<?php $catquery = new WP_Query( 'category_name=produtos' ); ?>
+<ul>
+<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
+<li><h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+<ul><li><?php the_content(); ?></li>
+</ul>
+</li>
+<?php endwhile; ?> 
+</ul>
+<?php wp_reset_postdata(); ?>
 </main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
