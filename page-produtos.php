3341f0db78dc85a357143352180588b766f61f65
Jumper Luko <jumper.luko@gmail.com>
2019-10-03 00:50:28 -0300

    Workin on Design again :) + title with animation + product list ~ better order on footer in mobile + simplest post template page + screenshot template
    > great to add more margin in contact form

--- page-produtos.php
+++ page-produtos.php
@@ -1,18 +1,20 @@
 <?php get_header(); ?>
 <link rel="stylesheet" type="text/css" href="<?php echo get_parent_theme_file_uri( 'css/produtos.min.css' );?>">
-<main class="">
-
-<!-- https://www.wpbeginner.com/wp-tutorials/how-to-display-recent-posts-from-a-specific-category-in-wordpress/ -->
-<?php $catquery = new WP_Query( 'category_name=produtos' ); ?>
-<ul>
-<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
-<li><h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-<ul><li><?php the_content(); ?></li>
-</ul>
-</li>
-<?php endwhile; ?> 
-</ul>
-<?php wp_reset_postdata(); ?>
+<h1 class="title-page"><div><span><?php the_title(); ?></span></div></h1>
+<main class="gcol-12">
+    <div class="produtos gcol-3">
+        <?php $catquery = new WP_Query( 'category_name=produtos&orderby=title&order=asc' );
+        while($catquery->have_posts()) : $catquery->the_post();
+            ?>
+            <a class="produto" href="<?php the_permalink() ?>" rel="bookmark">
+                <h2><?php the_title(); ?></h2>
+                <?php if( has_post_thumbnail() ):
+                echo get_the_post_thumbnail();
+                endif; 
+            echo '</a>';
+        endwhile; ?> 
+        <?php wp_reset_postdata(); ?>
+    </div>
 </main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
-<link rel="stylesheet" type="text/css" href="<?php echo get_parent_theme_file_uri( 'css/contato.min.css' );?>">
-<h1 class="title-page"><div><span><?php the_title(); ?></span></div></h1>
+<h1 class="title-page"><?php the_title(); ?></h1>
 <main class="">
     <?php
         if ( have_posts() ) : while ( have_posts() ) : the_post();
