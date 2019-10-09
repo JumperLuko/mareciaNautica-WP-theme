6620e105a3c298628fa66c0051144164989eb3b0
Jumper Luko <jumper.luko@gmail.com>
2019-10-08 23:51:44 -0300

    ~ little adjust 404 style ~ better html5 + filter on product + pages default

--- page-produtos.php
+++ page-produtos.php
@@ -1,18 +1,39 @@
 <?php get_header(); ?>
 <link rel="stylesheet" type="text/css" href="<?php echo get_parent_theme_file_uri( 'css/produtos.min.css' );?>">
-<h1 class="title-page"><div><span><?php the_title(); ?></span></div></h1>
+<header class="title-page"><div><h1><?php the_title(); ?></h1></div></header>
 <main class="gcol-12">
+    <div class="produtos-tags">
+        <div>
+            <a href="."><h2>Todos Produtos</h2></a>
+            <ul>
+                <?php
+                $tags = get_tags();
+                if ( $tags ) :
+                    foreach ( $tags as $tag ) : ?>
+                        <li><a href="?tag=<?php echo $tag->slug ?>" title="<?php echo esc_attr( $tag->name ); ?>"><?php echo esc_html( $tag->name ); ?></a></li>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
     <div class="produtos gcol-3">
-        <?php $catquery = new WP_Query( 'category_name=produtos&orderby=title&order=asc' );
-        while($catquery->have_posts()) : $catquery->the_post();
-            ?>
-            <a class="produto" href="<?php the_permalink() ?>" rel="bookmark">
-                <h2><?php the_title(); ?></h2>
-                <?php if( has_post_thumbnail() ):
-                echo get_the_post_thumbnail();
-                endif; 
-            echo '</a>';
-        endwhile; ?> 
+        <?php 
+        if($_GET["tag"] == null){
+            $catquery = new WP_Query( 'category_name=produtos&orderby=title&order=asc' );
+        }else{
+            $tagSlug = $_GET["tag"];
+            $catquery = new WP_Query( "category_name=produtos&orderby=title&order=asc&tag=$tagSlug" );
+        }
+            while($catquery->have_posts()) : $catquery->the_post();
+                ?>
+                <a class="produto" href="<?php the_permalink() ?>" rel="bookmark">
+                    <h2><?php the_title(); ?></h2>
+                    <?php if( has_post_thumbnail() ):
+                    echo get_the_post_thumbnail();
+                    endif; 
+                echo '</a>';
+            endwhile; 
+        ?> 
         <?php wp_reset_postdata(); ?>
     </div>
 </main>
--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <link rel="stylesheet" type="text/css" href="<?php echo get_parent_theme_file_uri( 'css/single.min.css' );?>">
-<h1 class="title-post"><?php the_title(); ?></h1>
+<header class="title-post"><h1><?php the_title(); ?></h1></header>
 <main class="">
     <div></div>
     <div class="content">
