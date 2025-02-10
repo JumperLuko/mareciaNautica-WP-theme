5f61dd7c34fbf28f917c748102ead25cda8bb8c3
JumperLuko <jumper.luko@gmail.com>
2025-02-10 17:23:24 -0300

    Simplificada na pagina de produtos Home com instagram ok

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
     <?php wp_footer(); ?>
-    <a href="https://api.whatsapp.com/send?phone=5547984790989&text=Ol%C3%A1,%20Estava%20visitando%20o%20site%20marecianautica.com.br.%20Est%C3%A1%20dispon%C3%ADvel?" class="whatsappFixo"><img src="<?php echo get_parent_theme_file_uri( '/img/icon/whatsapp.svg' );?>"/></a>
+    <a href="https://api.whatsapp.com/send?phone=5547984170451&text=Ol%C3%A1,%20Estava%20visitando%20o%20site%20marecianautica.com.br.%20Est%C3%A1%20dispon%C3%ADvel?" class="whatsappFixo"><img src="<?php echo get_parent_theme_file_uri( '/img/icon/whatsapp.svg' );?>"/></a>
     <footer class="gcol-b-2" title="Web Design feito por Thiago Augusto (jumperluko.com)">
         <a class="link1" href="/" >Página Inicial</a>
         <a class="contact1" href="/redes-sociais/" >Redes Sociais</a>
 
         <a class="link2" href="/como-escolher/" >Como Escolher</a>
-        <a class="contact2" href="https://api.whatsapp.com/send?phone=5547984790989&text=Ol%C3%A1,%20Estava%20visitando%20o%20site%20marecianautica.com.br.%20Est%C3%A1%20dispon%C3%ADvel?" target="_BLANK">Whatsapp: +55 47 98479-0989</a>
+        <a class="contact2" href="https://api.whatsapp.com/send?phone=5547989235805&text=Ol%C3%A1,%20Estava%20visitando%20o%20site%20marecianautica.com.br.%20Est%C3%A1%20dispon%C3%ADvel?" target="_BLANK">Whatsapp: +55 47 98923-5805</a>
 
         <a class="link3" href="/qualidade/" >Certificações</a>
         <a class="contact3" href="mailto:comercial.marecia@gmail.com">Email: comercial.marecia@gmail.com</a>
